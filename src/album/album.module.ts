25387e08b38e22795756316c0ac7573376e24223
Zheka <grushevskiy.yevgeniy@mail.ru>
2024-03-27 19:51:43 +0300

    feat: add global decorator for prisma module

--- src/album/album.module.ts
+++ src/album/album.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AlbumService } from './album.service';
 import { AlbumController } from './album.controller';
-import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [AlbumController],
   providers: [AlbumService],
 })
